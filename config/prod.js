945662ab7357336a240c17ab8c00d8a2c59b0b0a
GowthamGanapathiEMIS <Gowtham.Ganapathi@emishealth.com>
2020-09-03 14:48:58 +0530

    removed cmnds

--- config/prod.js
+++ config/prod.js
@@ -6,13 +6,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
-
-// module.exports = {
-//   googleClientID:
-//     "790056098429-amc6lsfk2h7ilv3ehq8r245fiajcbcpi.apps.googleusercontent.com",
-//   googleClientSecret: "vyeBu65DwWDaA0Xe0fVDypwN",
-//   mongoURI:
-//     "mongodb" +
-//     "+srv://emailyprod:uo9gif5Xl7SxHP1l@cluster0.qhmre.mongodb.net/emaily-prod?retryWrites=true&w=majority",
-//   cookieKey: "lkjhgfdssssssamnbvcxqwertyuiop",
-// };
